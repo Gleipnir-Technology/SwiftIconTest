d4d94f46d37359729d193e30ac8e18f500280427
Eli Ribble <eli@gleipnir.technology>
2025-08-12 08:28:31 -0700

    Initial Commit

---
+++ IconTester/IconTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconTesterApp.swift
+//  IconTester
+//
+//  Created by Eli Ribble on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct IconTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
